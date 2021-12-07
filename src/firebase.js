90c4515ddaa912e1b6517d5dfd4884e353167984
kimdlzp <kimdlzp@gmail.com>
2021-12-07 15:40:48 +0900

    firebase set

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB-qyGZwAURxbT2HPrNlpzJh5xnRek6kHg",
+  authDomain: "nwitter-adbe9.firebaseapp.com",
+  projectId: "nwitter-adbe9",
+  storageBucket: "nwitter-adbe9.appspot.com",
+  messagingSenderId: "469175312322",
+  appId: "1:469175312322:web:6a46d2dc52599555d3405b",
+};
+
+export default firebase.initializeApp(firebaseConfig);
